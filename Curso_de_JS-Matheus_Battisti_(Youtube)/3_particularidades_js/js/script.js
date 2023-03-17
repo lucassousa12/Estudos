5d9bdadbd74083a36a3e2bea2d73f4f21e08c01e
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-16 20:41:39 -0400

    Add - Aula Prática 3

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/3_particularidades_js/js/script.js
@@ -0,0 +1,27 @@
+//Executa o código de cima para baixo.
+console.log("Primeiro");
+
+console.log("Último");
+
+//Diferencia letra maiúsculas de minúsculas.
+console.log("É Case Sensistive");
+
+//Tipagem Fraca
+var a = 1;
+
+a = "Teste";
+
+console.log(a);
+
+//Ponto e vírgula (;) é opcional.
+console.log("; é Opcional")
+
+//  -   Comentário de uma Linha.
+
+/*
+    -   Comentário
+        de 
+        várias 
+        Linhas.
+
+*/
