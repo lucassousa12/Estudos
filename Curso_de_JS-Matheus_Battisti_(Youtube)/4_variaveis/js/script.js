87b3bfbcfbfe344a3c3109030d40f65f7964f256
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-16 21:20:36 -0400

    Add - Aula Prática 4 Sobre Variáveis.

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/4_variaveis/js/script.js
@@ -0,0 +1,22 @@
+//Não recomendado.
+nome = "João";
+
+//Símbolos Especiais aceitos são ($) e (_).
+//Não se pode iniciar com números.
+var $nome = "Vivian";
+var _nome = "Perebas";
+
+console.log(nome);
+
+//CamelCase - Primeira Letra do nome é Maiúscula.
+var meuPrimeiroNome = "Carlos";
+
+//Tipos de variáveis (VAR, LET, CONST).
+var teste = 1;
+let sobrenome = "Lucas";
+const pi = 3.14;
+
+//Javascript é de Tipagem Fraca.
+teste = "Lucas";
+
+console.log(teste);
