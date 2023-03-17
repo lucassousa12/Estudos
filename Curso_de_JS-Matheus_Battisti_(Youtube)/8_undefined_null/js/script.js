261a0a3bafece29a1a9a1f7fc6bf54fddec1e564
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-16 21:59:12 -0400

    Add - Aula prática 8 sobre Undefined e Null

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/8_undefined_null/js/script.js
@@ -0,0 +1,23 @@
+//Hoisting (Içamento) - As variáveis são incializadas no topo do código antes de rodar o programa.
+console.log(sobrenome);
+console.log(numero);
+
+
+//Null é quando a variável é inicializada vaziamente.
+var nome = null;
+
+console.log(nome);
+
+nome = "Lucas";
+
+console.log(nome);
+
+
+//Undefined é quando a variável é inicializada e não tem valor definido.
+var sobrenome;
+
+console.log(sobrenome);
+
+
+
+var numero = 12;
