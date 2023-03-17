fc3f4a881105ec5caf78105df4e06f3173896f9b
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-16 21:50:47 -0400

    Add - Aula prática 7 sobre Boolean

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/7_boolean/js/script.js
@@ -0,0 +1,17 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+
+/*  
+    Não se pode declarar variáveis com os nomes reservados.
+    var false = 3;
+*/
