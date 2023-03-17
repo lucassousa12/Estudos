aaa42319ff1e48ee78e88385971e13bef097d286
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-16 21:42:59 -0400

    Add - Aula prática 6 sobre Strings

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/6_string/js/script.js
@@ -0,0 +1,24 @@
+var nome =  "Lucas";
+var sobrenome = "Sousa";
+
+console.log(nome);
+console.log(typeof nome);
+
+//Usa-se o (+) para Concatenar Strings.
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+//Números dentro de aspas são considerados como Strings.
+console.log(typeof "1404");
+
+//Strings aceitam Aspas Simples ou Duplas.
+var frase = 'Está é uma frase completa';
+
+console.log(frase);
+console.log(typeof frase);
+
+//Adiciona o texto na Página.
+document.write("Olá 'Mundo'!");
+
+document.write('Olá "Mundo"!');
