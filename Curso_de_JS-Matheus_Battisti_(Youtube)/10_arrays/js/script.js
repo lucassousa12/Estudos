8bacf3d7c7e50bba0903e1b2113fb2fc37676999
Lucas Sousa <francisco1209lucas@gmail.com>
2023-03-20 22:21:08 -0400

    Add - Aula prática 10 sobre Arrays

---
+++ Curso_de_JS-Matheus_Battisti_(Youtube)/10_arrays/js/script.js
@@ -0,0 +1,19 @@
+//Cria um array.
+var arr = [5, "Lucas", {teste: true}];
+
+console.log(arr);
+
+var arr2 = [1,2,3,4,5,6];
+
+//Acessar um elemento específico do array.
+console.log(arr[1]);
+console.log(arr[0]);
+
+//Insere um elemento no array.
+arr[2] = 10;
+
+//Sobrescrever um elemento.
+arr[0] = "Vivian";
+
+console.log(arr);
+console.log(typeof arr);
